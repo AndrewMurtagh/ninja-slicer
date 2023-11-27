2535a6cbe57020f149f91d1a5be8d2b440e81d41
Andrew Murtagh <amurtagh09@gmail.com>
2023-11-27 09:52:44 +0000

    controlling camera position and adding zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,50 @@
 'use client';
 
-import React, { useRef, forwardRef, useCallback } from 'react';
-import * as Tabs from '@radix-ui/react-tabs';
+import { create } from 'zustand';
+import { useRef, forwardRef, useEffect } from 'react';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import { Box, OrbitControls } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import { Line } from '@react-three/drei';
-import { useDropzone } from 'react-dropzone';
 import * as THREE from 'three';
 
+
+type CameraPose = {
+    x: number;
+    y: number;
+    z: number;
+};
+
+
+type BuildSpaceDimensions = {
+    width: number;
+    depth: number;
+    height: number;
+};
+
+interface NinjaState {
+    cameraPose: CameraPose;
+    setCameraPose: (newPose: CameraPose) => void;
+    buildSpaceDimensions: BuildSpaceDimensions;
+}
+
+const INITIAL_BUILD_SPACE_DIMENSIONS: BuildSpaceDimensions = {
+    width: 200,
+    depth: 200,
+    height: 200
+};
+
+const ISOMETRIC_CAMERA_POSE: CameraPose = {
+    x: 50,
+    y: -50,
+    z: 100
+};
+
+const useNinjaStore = create<NinjaState>((set) => ({
+    cameraPose: ISOMETRIC_CAMERA_POSE,
+    setCameraPose: (newPose: CameraPose) => set((state) => ({ cameraPose: newPose })),
+    buildSpaceDimensions: INITIAL_BUILD_SPACE_DIMENSIONS
+}))
 
 
 const Axes = ({ length }: { length: number }) => {
@@ -21,17 +58,15 @@ const Axes = ({ length }: { length: number }) => {
 }
 
 
-
-
 const Scene = forwardRef((props: any, ref) => {
 
-    const width = props.buildSpaceDims[0];
-    const depth = props.buildSpaceDims[1];
-    const height = props.buildSpaceDims[2];
+    const buildSpaceDimensions = useNinjaStore(state => state.buildSpaceDimensions);
 
     const controlsRef = useRef<any>();
     const scene_ref = useRef<any>();
     const { camera, raycaster, gl } = useThree();
+    const cameraPose = useNinjaStore(state => state.cameraPose);
+
 
 
     useFrame(() => {
@@ -43,19 +78,21 @@ const Scene = forwardRef((props: any, ref) => {
     camera.near = 0.4;
     camera.far = 1000;
     camera.up.set(0, 0, 1);
-    camera.position.set(50, 10, 50);
 
+    useEffect(() => {
+        camera.position.set(cameraPose.x, cameraPose.y, cameraPose.z);
+    }, [cameraPose])
 
     return (
         <scene ref={scene_ref}>
             <ambientLight intensity={0.2} />
-            <pointLight intensity={0.2} position={[0, 0, height]} />
-            <pointLight intensity={0.2} position={[0, depth, height]} />
-            <pointLight intensity={0.2} position={[width, 0, height]} />
-            <pointLight intensity={0.2} position={[width, depth, height]} />
+            <pointLight intensity={0.2} position={[0, 0, buildSpaceDimensions.height]} />
+            <pointLight intensity={0.2} position={[0, buildSpaceDimensions.depth, buildSpaceDimensions.height]} />
+            <pointLight intensity={0.2} position={[buildSpaceDimensions.width, 0, buildSpaceDimensions.height]} />
+            <pointLight intensity={0.2} position={[buildSpaceDimensions.width, buildSpaceDimensions.depth, buildSpaceDimensions.height]} />
             <hemisphereLight intensity={0.8} groundColor={0x1e2424} position={[0, 0, 1]} />
             <OrbitControls
-                target={[width / 2, depth / 2, 0]}
+                target={[buildSpaceDimensions.width / 2, buildSpaceDimensions.depth / 2, 0]}
                 ref={controlsRef}
                 args={[camera, gl.domElement]}
                 enableRotate
@@ -67,26 +104,29 @@ const Scene = forwardRef((props: any, ref) => {
 });
 
 
-export const BuildSpace = ({ width, depth, height }: { width: number; depth: number; height: number; }) => {
+
+export const BuildSpace = () => {
+
+    const buildSpaceDimensions = useNinjaStore(state => state.buildSpaceDimensions);
 
     const DIVISIONS = 10;
     const xLines: any = [];
     const yLines: any = [];
 
-    for (let x = 0; x <= Math.floor(width / DIVISIONS); x++) {
-        xLines.push(<Line points={[[x * DIVISIONS, 0, 0], [x * DIVISIONS, depth, 0]]} color='aqua' lineWidth={1} />)
+    for (let x = 0; x <= Math.floor(buildSpaceDimensions.width / DIVISIONS); x++) {
+        xLines.push(<Line points={[[x * DIVISIONS, 0, 0], [x * DIVISIONS, buildSpaceDimensions.depth, 0]]} color='aqua' lineWidth={1} />)
     }
 
-    for (let y = 0; y <= Math.floor(depth / DIVISIONS); y++) {
-        yLines.push(<Line points={[[0, y * DIVISIONS, 0], [width, y * DIVISIONS, 0]]} color='aqua' lineWidth={1} />)
+    for (let y = 0; y <= Math.floor(buildSpaceDimensions.depth / DIVISIONS); y++) {
+        yLines.push(<Line points={[[0, y * DIVISIONS, 0], [buildSpaceDimensions.width, y * DIVISIONS, 0]]} color='aqua' lineWidth={1} />)
     }
 
     return (
         <>
             {xLines}
             {yLines}
-            <lineSegments position={[width / 2, depth / 2, height / 2]}>
-                <edgesGeometry attach='geometry' args={[new THREE.BoxGeometry(width, depth, height)]} />
+            <lineSegments position={[buildSpaceDimensions.width / 2, buildSpaceDimensions.depth / 2, buildSpaceDimensions.height / 2]}>
+                <edgesGeometry attach='geometry' args={[new THREE.BoxGeometry(buildSpaceDimensions.width, buildSpaceDimensions.depth, buildSpaceDimensions.height)]} />
                 <lineBasicMaterial color='aqua' attach='material' />
             </lineSegments>
         </>
@@ -95,26 +135,83 @@ export const BuildSpace = ({ width, depth, height }: { width: number; depth: num
 
 
 
+const enum CameraPreset {
+    Isometric = 'ISOMETRIC',
+    Left = 'LEFT',
+    Right = 'RIGHT',
+    Front = 'FRONT',
+    Top = 'TOP'
+};
+
+
 const Home = () => {
 
-    const camPos = {
-        x: 50,
-        y: 10,
-        z: 50
+
+    const setCameraPose = useNinjaStore(state => state.setCameraPose);
+    const buildSpaceDimensions = useNinjaStore(state => state.buildSpaceDimensions);
+
+    const cameraPresetChanged = (preset: string) => {
+
+        switch (preset) {
+            case CameraPreset.Isometric:
+                setCameraPose(ISOMETRIC_CAMERA_POSE);
+                break;
+
+            case CameraPreset.Left:
+                setCameraPose({
+                    x: -buildSpaceDimensions.width / 2,
+                    y: buildSpaceDimensions.depth / 2,
+                    z: buildSpaceDimensions.height / 2
+                });
+                break;
+
+            case CameraPreset.Right:
+                setCameraPose({
+                    x: buildSpaceDimensions.width + buildSpaceDimensions.width / 2,
+                    y: buildSpaceDimensions.depth / 2,
+                    z: buildSpaceDimensions.height / 2
+                });
+                break;
+
+            case CameraPreset.Front:
+                setCameraPose({
+                    x: buildSpaceDimensions.width / 2,
+                    y: -buildSpaceDimensions.depth / 2,
+                    z: buildSpaceDimensions.height / 2
+                });
+                break;
+
+            case CameraPreset.Top:
+                setCameraPose({
+                    x: buildSpaceDimensions.width / 2,
+                    y: buildSpaceDimensions.depth / 2,
+                    z: buildSpaceDimensions.height / 2
+                });
+                break;
+        }
     };
 
-    const buildSpaceDims = {
 
-        x: 200,
-        y: 200,
-        z: 200
-    }
 
 
     return (
         <div className='w-screen h-screen bg-zinc-950 grid grid-cols-5'>
 
             <div className=''>
+
+
+                <div className=''>
+
+
+                    <ToggleGroup.Root type='single' onValueChange={cameraPresetChanged}>
+                        <ToggleGroup.Item className='border border-red-400 text-white' value={CameraPreset.Isometric}>Isometric</ToggleGroup.Item>
+                        <ToggleGroup.Item className='border border-red-400 text-white' value={CameraPreset.Left}>Left</ToggleGroup.Item>
+                        <ToggleGroup.Item className='border border-red-400 text-white' value={CameraPreset.Right}>Right</ToggleGroup.Item>
+                        <ToggleGroup.Item className='border border-red-400 text-white' value={CameraPreset.Front}>Front</ToggleGroup.Item>
+                        <ToggleGroup.Item className='border border-red-400 text-white' value={CameraPreset.Top}>Top</ToggleGroup.Item>
+                    </ToggleGroup.Root>
+
+                </div>
 
             </div>
 
@@ -123,11 +220,12 @@ const Home = () => {
             <div className='col-span-3 border-l border-r border-zinc-600'>
 
                 <Canvas>
-                    <Scene buildSpaceDims={[buildSpaceDims.x, buildSpaceDims.y, buildSpaceDims.z]} cameraPos={[camPos.x, camPos.y, camPos.z]} />
+                    <Scene />
 
-                    <BuildSpace width={buildSpaceDims.x} depth={buildSpaceDims.y} height={buildSpaceDims.z} />
+                    <BuildSpace />
 
                     <Axes length={40} />
+
                 </Canvas>
             </div>
 
